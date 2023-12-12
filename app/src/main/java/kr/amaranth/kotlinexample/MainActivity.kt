d606c5064399a579d3f065ac6a7e96443b15668c
kimkyungho <dev_kimkyungho@kakao.com>
2023-12-12 14:43:00 +0900

    if / when

--- app/src/main/java/kr/amaranth/kotlinexample/MainActivity.kt
+++ app/src/main/java/kr/amaranth/kotlinexample/MainActivity.kt
@@ -62,6 +62,10 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, FunctionUsageActivity::class.java))
         }
 
+        binding.btnIntentControlFlow.setOnClickListener {
+            startActivity(Intent(this, ControlFlowActivity::class.java))
+        }
+
     }
 
     private fun openActivityResultLauncher(): ActivityResultLauncher<Intent> {
